28b3fcff75a22095f36af9e3ee1801f839217521
Starlight Romero <starlightromero@gmail.com>
2020-11-24 14:28:43 -0800

    finished basics example

--- redux-basics.js
+++ redux-basics.js
@@ -1,8 +1,31 @@
-// Store
+const redux = require('redux')
+const createStore = redux.createStore
+
+const initialState = {
+  counter: 0
+}
 
 // Reducer
+const rootReducer = (state = initialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {...state, counter: state.counter + 1}
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {...state, counter: state.counter + action.value}
+  }
+  return state
+}
 
-// Dispatching Action
+// Store
+const store = createStore(rootReducer)
+console.log(store.getState())
 
 // Subscription
+store.subscribe(() => {
+  console.log(store.getState())
+})
 
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'})
+store.dispatch({type: 'ADD_COUNTER', value: 10})
+console.log(store.getState())
