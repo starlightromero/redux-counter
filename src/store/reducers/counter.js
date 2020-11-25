81b3778ce74823e96c4b9d4069b3ddcde42c09f9
Starlight Romero <starlightromero@gmail.com>
2020-11-25 08:51:01 -0800

    broke reducer up into two reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions'
+import * as actionTypes from '../actions'
 
 const initialState = {
-  counter: 0,
-  results: []
+  counter: 0
 }
 
 const reducer = (state = initialState, action) => {
@@ -26,17 +25,6 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         counter: state.counter - action.value
-      }
-    case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({id: new Date(), value: state.counter})
-      }
-    case actionTypes.DELETE_RESULT:
-      const updatedResults = state.results.filter(result => result.id !== action.resultId)
-      return {
-        ...state,
-        results: updatedResults
       }
     default:
       return state
