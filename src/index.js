7cfaa05b8dd075086533a98e0855eb16d166cd48
Starlight Romero <starlightromero@gmail.com>
2020-11-25 08:50:47 -0800

    set up combineReducers

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from './store/reducer'
+import { createStore, combineReducers } from 'redux'
+import counterReducer from './store/reducers/counter'
+import resultReducer from './store/reducers/result'
 
-const store = createStore(reducer)
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+})
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
