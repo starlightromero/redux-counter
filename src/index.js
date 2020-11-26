ed70303516817c9c95e6b5c83a31b8af5d00be97
Starlight Romero <starlightromero@gmail.com>
2020-11-25 21:17:28 -0800

    added middleware and set up Redux Devtools

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { Provider } from 'react-redux'
+import { Provider, applyMiddleware, compose } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
 import counterReducer from './store/reducers/counter'
 import resultReducer from './store/reducers/result'
@@ -13,7 +13,20 @@ const rootReducer = combineReducers({
   res: resultReducer
 })
 
-const store = createStore(rootReducer)
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action)
+      const result = next(action)
+      console.log('[Middleware] next state', store.getState())
+      return result
+    }
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)))
 
 ReactDOM.render(
   <React.StrictMode>
