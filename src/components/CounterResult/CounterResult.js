d27d139578cf4a552c2c4fc27b0e33e939ec1d8c
Starlight Romero <starlightromero@gmail.com>
2020-11-25 22:36:43 -0800

    created CounterResult

---
+++ src/components/CounterResult/CounterResult.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './CounterResult.module.css'
+import PropTypes from 'prop-types'
+
+const CounterResult = props => (
+  <div className={classes.CounterResult} onClick={props.clicked}>
+    {props.value}
+  </div>
+)
+
+CounterResult.propTypes = {
+  value: PropTypes.int,
+  clicked: PropTypes.func
+}
+
+export default CounterResult
