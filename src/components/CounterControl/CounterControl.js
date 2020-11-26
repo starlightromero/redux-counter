7646b51ae868758c62469c3d73eab6ffa72e80e1
Starlight Romero <starlightromero@gmail.com>
2020-11-25 22:36:55 -0800

    added prop types

--- src/components/CounterControl/CounterControl.js
+++ src/components/CounterControl/CounterControl.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import classes from './CounterControl.module.css'
+import PropTypes from 'prop-types'
 
-const counterControl = (props) => (
+const CounterControl = props => (
   <div className={classes.CounterControl} onClick={props.clicked}>
     {props.label}
   </div>
 )
 
-export default counterControl
+CounterControl.propTypes = {
+  label: PropTypes.string,
+  clicked: PropTypes.func
+}
+
+export default CounterControl
